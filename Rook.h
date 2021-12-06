0cd5c89e2827111364ee6dcd774fb56a9df98ecf
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2021-12-06 22:06:32 +0200

    declared class Rook

--- Rook.h
+++ Rook.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "Piece.h"
-class Rook :
-    public Piece
+class Rook : public Piece
 {
+public:
+	int validMove(const Board& board, const Position& src, const Position& dst) const override;
 };
-
