93efa944019da697966e70facf6e1092c66cd0b9
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2021-12-06 23:22:01 +0200

    declared class Board

--- Board.h
+++ Board.h
@@ -1,5 +1,19 @@
 #pragma once
+#include "Position.h"
+#include "Piece.h"
+#include<iostream>
+
+#define BOARD_SIZE 8
+
 class Board
 {
+private:
+	Piece* _board[BOARD_SIZE][BOARD_SIZE];
+public:
+	Board(const std::string& formatString);
+	~Board();
+	void movePiece(const Position& src, const Position& dst);
+	std::string getBoard();
+	Piece* operator[] (const Position&);
 };
 
