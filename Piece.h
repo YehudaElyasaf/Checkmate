089072486730ef3c1505974733f6a7bc509ed739
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2021-12-06 21:59:26 +0200

    declared class Piece

--- Piece.h
+++ Piece.h
@@ -1,5 +1,14 @@
 #pragma once
+
+#define WHITE 0
+#define BLACK 1
+
 class Piece
 {
+private:
+	bool _color;
+public:
+	virtual ~Piece();
+	virtual int validMove(const Board& board, const Position& src, const Position& dst) const = 0 ;
 };
 
