e9fbaf963b77f061ce3caf3832b2db327148d35e
gavrielhahn@gmail.com <gavrielhahn@gmail.com>
2021-12-07 00:20:10 +0200

    Position class

--- Position.cpp
+++ Position.cpp
@@ -1,1 +1,22 @@
 #include "Position.h"
+
+Position::Position(const std::string& posString)
+{
+	_x = posString[0] - 'a';
+	_y = posString[1] - '1';
+}
+
+int Position::getX() const
+{
+	return _x;
+}
+
+int Position::getY() const
+{
+	return _y;
+}
+
+bool Position::operator==(const Position& other) const
+{
+	return this->_x == other._x && this->_y == other._y;
+}
--- Position.h
+++ Position.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <iostream>
 class Position
 {
+public:
+	Position(const std::string& posString);
+	int getX() const;
+	int getY() const;
+	bool operator==(const Position& other) const;
+private:
+	int _x;
+	int _y;
 };
-
