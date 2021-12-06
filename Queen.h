4d2ccbd9953e5b583337c451a7f13f2cf07ade43
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2021-12-07 00:08:55 +0200

    declared class queen

--- Queen.h
+++ Queen.h
@@ -1,7 +1,11 @@
 #pragma once
-#include "Piece.h"
-class Queen :
-    public Piece
+
+#include "Bishop.h"
+#include "Rook.h"
+
+class Queen :public Bishop,public Rook
 {
+	Queen(bool color);
+	bool validMove(const Position& src, const Position& dst) const override;
 };
 
