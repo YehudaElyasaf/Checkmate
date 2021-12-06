db64a7ba631e4505176ac6cd50c3eccb0b09b467
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2021-12-07 00:07:01 +0200

    added Bishops c'tor

--- Bishop.cpp
+++ Bishop.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 Bishop::Bishop(bool color) :Piece(color) {}
+
 bool Bishop::validMove(const Position& src, const Position& dst) const {
 	return std::abs(src.getX() - dst.getX()) ==
 		std::abs(src.getY() - dst.getY());
