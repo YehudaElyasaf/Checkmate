3d104f128109266b33b9d81866bb58e99aeea7cb
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2021-12-06 23:13:51 +0200

    wrote function Bishop::validMove

--- Bishop.cpp
+++ Bishop.cpp
@@ -1,1 +1,7 @@
 #include "Bishop.h"
+#include <cstdlib>
+
+bool Bishop::validMove(const Position& src, const Position& dst) const {
+	return std::abs(src.getX() - dst.getX()) ==
+		std::abs(src.getY() - dst.getY());
+}
--- Bishop.h
+++ Bishop.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "Piece.h"
-class Bishop :
-    public Piece
-{
-};
 
+class Bishop : public Piece
+{
+public:
+	bool validMove(const Position& src, const Position& dst) const override;
+};
