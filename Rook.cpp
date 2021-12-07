971a6eb5c5d65ee9a0ea9bc498ad0624156144df
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2021-12-07 08:35:16 +0200

    check

--- Rook.cpp
+++ Rook.cpp
@@ -5,4 +5,5 @@ Rook::Rook(bool color) :Piece(color) {}
 bool Rook::validMove(const Position& src, const Position& dst) const {
 	return src.getX() == dst.getX() ||
 		src.getY() == dst.getY();
-}
+} 
+
