ffdcb8fff0be41cadd3205cfc0153336ab2dc4ef
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2021-12-07 23:40:10 +0200

    updated function Rook::validMove to check if the path is empty

--- Rook.cpp
+++ Rook.cpp
@@ -3,9 +3,38 @@
 Rook::Rook(bool color) :Piece(color) {}
 
 bool Rook::validMove(const Board& board, const Position& src, const Position& dst) const {
-	return src.getX() == dst.getX() ||
-		src.getY() == dst.getY();
-} 
+	if (src.getX() == dst.getX())
+	{
+		if (src.getY() < dst.getY()) {
+			for (int i = src.getY() + 1; i < dst.getY(); i++) {
+				if (board[Position(src.getX(), i)] != nullptr)
+					return false;
+			}
+		}
+		else {
+			for (int i = src.getY() - 1; i > dst.getY(); i--) {
+				if (board[Position(src.getX(), i)] != nullptr) 
+					return false;
+			}
+		}
+	}
+	else if (src.getY() == dst.getY()) {
+		if (src.getX() < dst.getX()) {
+			for (int i = src.getX() + 1; i < dst.getX(); i++) {
+				if (board[Position(i, src.getY())] != nullptr)
+					return false;
+			}
+		}
+		else {
+			for (int i = src.getX() - 1; i > dst.getX(); i--) {
+				if (board[Position(i, src.getY())] != nullptr)
+					return false;
+			}
+		}
+	}
+
+	return false;
+}
 
 char Rook::getType() const {
 	if (this->_color == WHITE)
