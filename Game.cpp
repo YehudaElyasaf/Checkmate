3b6f6738e1d1579ab1cf37358f6c0ae7ab6ce189
gavrielhahn@gmail.com <gavrielhahn@gmail.com>
2021-12-07 15:17:51 +0200

    Created Game c'tors

--- Game.cpp
+++ Game.cpp
@@ -1,12 +1,9 @@
 #include "Game.h"
+#define BOARD_STRING_END 64
 
-Game::Game(const string& formatString)
-{
-}
+Game::Game(const string& formatString): _board(formatString.substr(0, BOARD_STRING_END)), _turn(formatString[BOARD_STRING_END] - '0') {}
 
-Game::Game()
-{
-}
+Game::Game(): Game("RNBKQBNRPPPPPPPP################################pppppppprnbkqbnr1") {}
 
 int Game::move(const string& moveString)
 {
