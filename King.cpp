a5e476876e0456719234ebeafe9f40c28215277f
gavrielhahn@gmail.com <gavrielhahn@gmail.com>
2021-12-07 11:50:59 +0200

    finished King class

--- King.cpp
+++ King.cpp
@@ -1,1 +1,18 @@
 #include "King.h"
+#include <math.h>
+
+King::King(bool color) : Piece(color) {}
+
+bool King::validMove(const Position& src, const Position& dest) const
+{
+	return abs(src.getX() - dest.getX()) <= 1 && abs(src.getY() - dest.getY()) <= 1;
+}
+
+char King::getType() const
+{
+	if (_color == WHITE)
+	{
+		return 'K';
+	}
+	return 'k';
+}
--- King.h
+++ King.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "Piece.h"
+#include "Position.h"
 class King :
     public Piece
 {
+public:
+    King(bool color);
+    bool validMove(const Position& src, const Position& dest) const override;
+    char getType() const override;
 };
 
