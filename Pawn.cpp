088676be4c5190c6fa1a859efb6007c67749abb9
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2021-12-08 00:15:09 +0200

    flipped board because the white is in the bottom side

--- Pawn.cpp
+++ Pawn.cpp
@@ -1,8 +1,9 @@
 #include "Pawn.h"
 #include <math.h>
-#define WHITE_START 7
-#define BLACK_START 2
-Pawn::Pawn(bool color): Piece(color) {}
+#define WHITE_START 1
+#define BLACK_START 6
+
+Pawn::Pawn(bool color) : Piece(color) {}
 
 bool Pawn::validMove(const Board& board, const Position& src, const Position& dest) const
 {
@@ -14,9 +15,11 @@ bool Pawn::validMove(const Board& board, const Position& src, const Position& de
 	{
 		if (_color == WHITE)
 		{
-			return ((src.getY() == WHITE_START && dest.getY() == WHITE_START - 2) || src.getY() - dest.getY() == 1);
+			return ((src.getY() == WHITE_START && dest.getY() == WHITE_START + 2) || (src.getY() - dest.getY() == 1));
 		}
-		return ((src.getY() == BLACK_START && dest.getY() == BLACK_START + 2) || src.getY() - dest.getY() == -1);
+		else { //color == BLACK
+			return ((src.getY() == BLACK_START && dest.getY() == BLACK_START - 2) || (src.getY() - dest.getY() == -1));
+		}
 	}
 }
 
