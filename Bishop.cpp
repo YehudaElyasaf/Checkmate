4caeb01091dbf04174c85d38a279abf35a073518
328193602 <hvusvvnkl12345@gmail.com>
2021-12-13 08:18:30 -0800

    wrote function Bishop::validMove

--- Bishop.cpp
+++ Bishop.cpp
@@ -1,13 +1,40 @@
 #include "Bishop.h"
-#include <cstdlib>
 
 Bishop::Bishop(bool color) :Piece(color) {}
 
 bool Bishop::validMove(const Board& board, const Position& src, const Position& dst) const {
-	return std::abs(src.getX() - dst.getX()) ==
-		std::abs(src.getY() - dst.getY());
+	Position startCheck(0, 0);
+	Position endCheck(0, 0);
+
+	if (src.getY() < dst.getY()) {
+		startCheck = src;
+		endCheck = dst;
+	}
+	else {
+		startCheck = dst;
+		endCheck = src;
+	}
+
+	if (src.getX() - dst.getX() == src.getY() - dst.getY()) {
+		//move in / shape
+		for (int i = 1; startCheck.getX() + i < endCheck.getX(); i++) {
+			if (board[Position(startCheck.getX() + i, startCheck.getY() + 1)] != nullptr)
+				return false;
+		}
+		return true;
+	}
+	else if (src.getX() - dst.getX() == (src.getY() - dst.getY()) * REMOVE_ZERO) {
+		//move in \ shape
+
+		for (int i = 1; startCheck.getX() - i < endCheck.getX(); i--) {
+			if (board[Position(startCheck.getX() - i, startCheck.getY() + 1)] != nullptr)
+				return false;
+		}
+		return true;
+	}
+	return false;
 }
-char Bishop::getType() const{
+char Bishop::getType() const {
 	if (this->_color == WHITE)
 		return BISHOP_WHITE;
 	else
